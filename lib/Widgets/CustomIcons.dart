8a8a3106d18620c32c57d475639859f5caba1a98
yanjiepeng <382878591@qq.com>
2019-10-17 14:15:27 +0800

    Initial commit

---
+++ lib/Widgets/CustomIcons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static final IconData twitter = IconData(0xe900, fontFamily: "CustomIcons");
+  static final IconData facebook = IconData(0xe901, fontFamily: "CustomIcons");
+  static final IconData googlePlus =
+      IconData(0xe902, fontFamily: "CustomIcons");
+  static final IconData Linkedin = IconData(0xe903, fontFamily: "CustomIcons");
+}
